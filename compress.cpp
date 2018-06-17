5f285f9d4610ed2d96681b4e2babee652c458742
a-elk <adhamelkarn@hotmail.com>
2018-06-17 22:37:07 +0200

    Add files via upload

--- compress.cpp
+++ compress.cpp
@@ -5,109 +5,119 @@
 #include <map>
 #include <bitset>
 #include <typeinfo>
+#include <cmath>
 
 using namespace std;
-map<string,short> create_dict(){
-  map<string,short> dict;
+
+void create_dict(map<string,int>& dict){
   int a = 0;
   while(a < 256){
-    //cout << "a : " << (int)a << endl;
     dict.emplace(string(1,a),a);
     a++;
   }
-  return dict;
-}
-void print_str(string& str,char* name){
-  for(int i= 0; i < str.size();++i){
-    cout << name <<"[" << i << "] : " <<str[i] << " ";
-  }
-  cout <<"\n";
-}
-void print_dict(map<string,short>& dict){
-  map<string,short>::iterator it= dict.begin();
-
 }
 
-void print_map(map<string,short>& mymap){
-  map<string,short>::iterator it=mymap.begin();
-  while(it != mymap.end()){
-    cout << "map["<<it->first<<"] : "<<it->second<<endl;
-    ++it;
-  }
+void clear_dict(map<string,int>& dict){
+  dict.clear();
+  create_dict(dict);
 }
+
+vector<unsigned char> pack(vector<int>& unpacked_buff){
+  int var_width = 9;
+  vector<unsigned char> packed_buff;
+  int reste;
+  int n = var_width;
+  int i,j;
+  unsigned char to_push;
+  int size = 9;
+  for(i = 0,j = 256; i < unpacked_buff.size()+1;++i,++j){
+    if(j > (pow(2,var_width)-1)){
+      ++var_width;
+    }
+    if(j > (pow(2,13)-2)){
+      var_width = 9;
+      j = 256;
+    }
+    to_push = unpacked_buff[i] >> (n - 8);
+    packed_buff.push_back(to_push);
+    reste = unpacked_buff[i] - (packed_buff.back()<<(n - 8));
+    if(n >= 17){
+      unpacked_buff[i] = reste;
+      n -= 8;
+      if(i+1 >= unpacked_buff.size()+1){
+	break;
+      }
+      --i;
+      --j;
+    }
+    else{
+      if(j+1 > (pow(2,var_width)-1))
+	++var_width;
+      if(j+1 > (pow(2,13)-2)){
+	var_width = 9;
+	j = 255;
+      }
+      unpacked_buff[i+1] = reste << var_width | unpacked_buff[i+1];
+      n = var_width + (n - 8);
+      
+    }
+  }
+  return packed_buff;
+}
+
+
 
 void compress(char* myfile,char* file_created){
-  map<string,short> dict;
-  dict = create_dict();
-  //  print_map(dict);
+  map<string,int> dict;
+  create_dict(dict);
   ifstream file(myfile, ios::binary | ::ios::ate);
   streamsize size = file.tellg();
   file.seekg(0, ios::beg);
   vector<unsigned char> buffer(size);
+  vector<int>compressed_file;
   if (file.read((char*)buffer.data(), size))
     {
       ofstream file2;
       file2.open(file_created);
       string w = "";
       vector<unsigned char>::const_iterator i = buffer.begin();
-      int nb(256);
+      int nb(257);
       string c;
       string p;
-      while(i < buffer.end()){
-        cout << "*i : " <<hex<<(int)*i << endl;
-        c.assign(1,*i);
-        cout << "c : " <<c << endl;
-        print_str(c,(char*)"c");
-        //cout << "c : "<<(short)c[0]<<endl;
-        //cout << "w : "<<w<<endl;
-        p = w + c;
-        print_str(p,(char*)"p");
-        map<string,short>::iterator f;
-        f = dict.find(p);
-        if(f != dict.end()){
-          //cout << "dict[p] : " << dict[p] << endl;
-          w = p;
-          cout << "found w : "<<w<<endl;
-        }
-        else{
-          dict.emplace(p,nb);
-          //cout << "dict[w] : " <<dict[w]<< endl;
-          for (int i = 0; i < w.size();++i){
-            cout << "w["<< i << "]  : " <<w[i]<<endl;
-          }
-          short val = dict[w];
-          cout << "val : "<< val << endl;
-          //bitset<16> x(val);
-          //cout << "Res : " << x << endl;
-          //x = x.to_ulong();
-          bitset<16> x(val);
-          //cout << "debug : " << x << endl;
-          if(val < 256){
-            if(val > 64 && val < 127)//cout << "1 a ecrire : " << x << endl;
-              cout << "1 a ecrire : " << val << endl;
-            else
-              cout << "1 a ecrire : " << val << endl;
-            file2.write((char*)&val,2);
-          }
-          else{
-            cout << "a ecrire : " << val << endl;
-            file2.write((char*)&val,2);
-          }
+      while(i != buffer.end()){
+	if(nb > pow(2,13)-2){
+	  clear_dict(dict);
+	  compressed_file.push_back(256);
+	  nb = 257;
+	}
+	c.assign(1,*i);
+	p = w + c;
+	map<string,int>::iterator f;
+	f = dict.find(p);
+	if(f != dict.end())
+	  w = p;
+	else{
+	  dict.emplace(p,nb);
+          int val = dict[w];
+	  compressed_file.push_back(val);
           w = c;
-          // if(nb < (2^16))
           ++nb;
-          cout << "nb : "<< nb << endl;
         }
+	if(i + 1 == buffer.end()){
+	  if(nb > pow(2,13)-2)
+	    compressed_file.push_back(256);
+	  compressed_file.push_back(dict[w]);
+	  break;
+	}
         ++i;
       }
+      vector<unsigned char> packed_buff = pack(compressed_file);
+      file2.write((char*)&packed_buff[0],packed_buff.size());
     }
-  //print_map(dict);
-    //print_dict(dict);
+  else
+    cout << "Ficher non ouvert" << endl;
 }
 
 int main(int argc,char* argv[]){
   compress(argv[1],argv[2]);
-  /*short a(224);
-  bitset<16>x(a);
-  cout << "x : " << x << endl;*/
 }
--- decompress.cpp
+++ decompress.cpp
@@ -3,91 +3,110 @@
 #include <string>
 #include <fstream>
 #include <map>
+#include <cmath>
 #include <bitset>
-#include <typeinfo>
 
 using namespace std;
-map<short,string> create_dict(){
-  map<short,string> dict;
+
+void create_dict(map<int,string>& dict){
   int a = 0;
   while(a < 256){
     dict.emplace(a,string(1,a));
-    a++;
+    ++a;
   }
-  return dict;
 }
 
+void clear_dict(map<int,string>& dict){
+  dict.clear();
+  create_dict(dict);
+}
+
+vector<int> unpack(vector<unsigned char>& packed_buff){
+  int var_width = 9;
+  vector<int>upacked2;
+  for(int k = 0 ; k < packed_buff.size();++k)
+    upacked2.push_back(packed_buff[k]);
+  vector<int> unpacked_buff;
+  int reste;
+  int n = var_width;
+  int i,j;
+  int to_push;
+  int size = 8;
+  for(i = 0, j = 256; i < upacked2.size()-1;++i){
+    if(j > (pow(2,13)-2)){
+      var_width = 9;
+      j = 256;
+    }
+    if(j > (pow(2,var_width)-1))
+      ++var_width;
+    if((8 - (var_width -size))< 0){
+      upacked2[i+1] = upacked2[i] << 8 | upacked2[i+1];
+      size = 8 + size;
+      ++i;
+    }
+    reste = upacked2[i+1] >> (8 - (var_width - size));
+    to_push = upacked2[i] << (var_width - size) | reste;
+    unpacked_buff.push_back(to_push);
+    ++j;
+    upacked2[i+1] = upacked2[i+1] - (reste << (8 - (var_width - size)));
+    if((8 - (var_width - size))<= 0){
+      ++i;
+      size = 8;
+    }
+    else
+      size = 8 - (var_width - size);
+  }
+  return unpacked_buff;
+}
+
+
+
 void decompress(char* myfile,char* file_created){
-  map<short,string> dict;
-  dict = create_dict();
+  map<int,string> dict;
+  create_dict(dict);
   fstream file(myfile,ios_base::in);
-  //streamsize size = file.tellg();
-  //file.seekg(0, ios::beg);
-  vector<short> buffer;
+  vector<unsigned char>packed_buff;
   if (file.is_open())
     {
-      short value;
-      while(file.read(reinterpret_cast<char *>(&value), sizeof(value))){
-//        cout <<"value : "<<(char)value << endl;
-        buffer.push_back(value);
+      unsigned char value;
+      while(file.read(reinterpret_cast<char *>(&value),sizeof(value))){
+        packed_buff.push_back(value);
       }
-      vector<short>::const_iterator it = buffer.begin();
+      vector<int> buffer = unpack(packed_buff);
+      vector<int>::const_iterator it = buffer.begin();
+      int v = *it;
       ofstream file2;
       file2.open(file_created);
-      short v = *it;
-      cout << "ecris: "<< dict[v] << endl;
-      file2.write(&dict[v][0], sizeof(char));
+      file2.write((char*)&dict[v][0],sizeof(char));
+      string to_write;
       string w;
       w.assign(1,*it);
-      int nb(256);
-      for(unsigned int i = 2; i <buffer.size();++i){
-        it++;
+      int nb = 257;
+      ++it;
+      while(it != buffer.end()){
         v = *it;
-        cout << "v : "<< v << endl;
-        //cout << "dict[v] : "<< dict[v] << endl;
-        //cout << "w : "<< w << endl;
-        map<short,string>::iterator f;
+	if(v == 256){
+	  clear_dict(dict);
+	  nb = 256;
+	  it++;
+	  w = "";
+	  v = *it;
+	}
+        map<int,string>::iterator f;
         f = dict.find(v);
-        string to_write;
-        if(f != dict.end()){
-          cout << "f->first: " << f->first << endl;
-          cout << "f->second: " << hex << f->second << endl;
-   //       cout << "dict[v]: " << dict[v] << endl;
+        if(f != dict.end())
           to_write = f->second;
-          //cout << "if to_write : " << &to_write[0] << endl;
-        }
-        else{
-          cout << "not_in dict" << endl;
-          to_write = w + w[0];
-          cout << "w : " << w <<"w[0] : "<<w[0]<< endl;
-        }
-        if(to_write.length() == 1){
-          cout << "to_write.length(): "<<to_write.length()<<endl;
-          //cout << "brut :  " << to_write << endl;
-          //cout << "1 ecris: "<< hex << (int)to_write[0] << endl;
-          cout << "1 ecris: "<< (char)to_write[0] << endl;
-          file2.write(&to_write[0],to_write.length());
-        }
-        else{
-          cout << "to_write.length(): "<<to_write.length()<<endl;
-          for(unsigned int i = 0; i < to_write.length();++i){
-            //cout << "to_write["<<i<<"] : "<<hex<<(int)to_write[i]<<" ";
-            cout << "to_write["<<i<<"] : "<<to_write[i]<<" ";
-            cout << endl;
-          }
-          //cout << "ecris: "<< hex << to_write << endl;
-          cout << endl;
-          file2.write(&to_write[0],to_write.length());
-        }
-        dict.emplace(nb,w + to_write[0]);
-        w = to_write;
-        ++nb;
-        }
+	else
+          to_write= w + w[0];
+	file2.write(&to_write[0],to_write.size());
+	dict.emplace(nb,w + to_write[0]);
+	w = to_write;
+	++nb;
+	++it;
       }
+    }
 }
 
 int main(int argc,char* argv[]){
   decompress(argv[1],argv[2]);
 }
-
-
